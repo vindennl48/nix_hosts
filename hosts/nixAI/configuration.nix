4df4ab78b03a95be2f9550f873d83963ccf783d6
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2025-02-18 21:55:05 -0500

    Fixed ollama-overlay; works with specific versions now

--- hosts/nixAI/configuration.nix
+++ hosts/nixAI/configuration.nix
@@ -12,7 +12,8 @@ let
   unstable = import (pkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "1da52dd49a127ad74486b135898da2cef8c62665";  # Get latest from https://status.nixos.org/
+    # rev = "1da52dd49a127ad74486b135898da2cef8c62665";  # Get latest from https://status.nixos.org/
+    rev = "f0204ef4baa3b6317dee1c84ddeffbd293638836";  # Get latest from https://status.nixos.org/
     sha256 = "sha256-KRwX9Z1XavpgeSDVM/THdFd6uH8rNm/6R+7kIbGa+2s="; # Get from error message when you try with wrong hash
   }) { config = config.nixpkgs.config; };
 
@@ -28,15 +29,13 @@ in
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/nvidia.nix
       /etc/nixos/hardware-configuration.nix
+      /etc/nixos/nvidia.nix
     ];
 
   nixpkgs.overlays = [
-    (self: super: {
-      lmstudio39 = super.callPackage /etc/nixos/lmstudio.nix { };
-    })
-    # (import /etc/nixos/ollama-overlay.nix)
+    # (self: super: { lmstudio39 = super.callPackage /etc/nixos/lmstudio.nix { }; })
+    (import /etc/nixos/ollama-overlay.nix)
   ];
 
   # List packages installed in system profile. To search, run:
@@ -48,46 +47,52 @@ in
      less
      wget
      nh
-     nvidia-docker
-     python311
+     # nvidia-docker
+     python312
      # lmstudio39 # from overlay
-     # ollama-fix # use docker
+     ollama-overlay # use docker
   ];
+
+  environment.sessionVariables = {
+    # needed for open-webui compile
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  };
 
   programs = {
     zsh.enable = true;
     firefox.enable = true;
   };
 
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-    # enableNvidia = true; # above 24.05 use hardware.nvidia-container.toolkit below
-  };
-  # also need pkg.nvidia-docker
-  hardware.nvidia-container-toolkit.enable = true; # 24.11+
-
-  # docker containers
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers = {
-      open-webui = {
-        image = "ghcr.io/open-webui/open-webui:ollama";
-        ports = ["0.0.0.0:3000:8080"];
-        environment = {
-          OLLAMA_MAX_LOADED_MODELS = "1";
-          OLLAMA_NUM_PARALLEL = "1";
-          OLLAMA_MAX_QUEUE = "1";
-        };
-        volumes = [
-          "/var/lib/ollama:/root/.ollama"
-          "/var/lib/open-webui:/app/backend/data"
-        ];
-        extraOptions = [ "--device=nvidia.com/gpu=all" ];
-        autoStart = true;
-      };
-    };
-  };
+  ## SET UP AUTO DOCKER CONTAINER ##
+  # virtualisation.docker = {
+  #   enable = true;
+  #   enableOnBoot = true;
+  #   # enableNvidia = true; # above 24.05 use hardware.nvidia-container.toolkit below
+  # };
+  # # also need pkg.nvidia-docker
+  # hardware.nvidia-container-toolkit.enable = true; # 24.11+
+  #
+  # # docker containers
+  # virtualisation.oci-containers = {
+  #   backend = "docker";
+  #   containers = {
+  #     open-webui = {
+  #       image = "ghcr.io/open-webui/open-webui:ollama";
+  #       ports = ["0.0.0.0:3000:8080"];
+  #       environment = {
+  #         OLLAMA_MAX_LOADED_MODELS = "1";
+  #         OLLAMA_NUM_PARALLEL = "1";
+  #         # OLLAMA_MAX_QUEUE = "1";
+  #       };
+  #       volumes = [
+  #         "/var/lib/ollama:/root/.ollama"
+  #         "/var/lib/open-webui:/app/backend/data"
+  #       ];
+  #       extraOptions = [ "--device=nvidia.com/gpu=all" ];
+  #       autoStart = true;
+  #     };
+  #   };
+  # };
 
   # Bootloader
   boot = {
--- hosts/nixAI/ollama-overlay.nix
+++ hosts/nixAI/ollama-overlay.nix
@@ -1,18 +1,20 @@
 self: super: {
-  ollama-fix = super.stdenv.mkDerivation rec {
+  ollama-overlay = super.stdenv.mkDerivation rec {
     pname = "ollama-cuda";
-    version = "latest";
+    version = "0.5.11";
 
-    src = super.fetchurl {
-      url = let
-        arch = super.stdenv.hostPlatform.uname.processor;
-        mappedArch = {
-          "x86_64" = "amd64";
-          "aarch64" = "arm64";
-        }.${arch} or (throw "Unsupported architecture: ${arch}");
-      in "https://ollama.com/download/ollama-linux-${mappedArch}.tgz";
+    src = let
+      arch = super.stdenv.hostPlatform.uname.processor;
+      mappedArch = {
+        "x86_64" = "amd64";
+        "aarch64" = "arm64";
+      }.${arch} or (throw "Unsupported architecture: ${arch}");
+    in super.fetchurl {
+      url = "https://ollama.com/download/ollama-linux-${mappedArch}.tgz?version=${version}";
+      # in "https://ollama.com/download/ollama-linux-${mappedArch}.tgz";
       # Replace with actual hash using nix-prefetch-url
-      sha256 = "sha256-jU4FS8US1TEVB0wZ3lp5Fd94GA+JwBSruuRJfkqBOjw=";
+      sha256 = "sha256-qjhs4cMUaGEz96vptWHz7yMPA/iLvdH1g8lRxKtTeK0=";
+      name = "ollama-linux-${mappedArch}-${version}.tgz";
     };
 
     nativeBuildInputs = [
@@ -38,7 +40,7 @@ self: super: {
     unpackPhase = ''
       runHook preUnpack
       mkdir -p $out
-      tar xzf $src -C $out --strip-components=1
+      tar xzf $src -C $out --strip-components=0
       # Remove ROCm components if only CUDA is needed
       rm -rf $out/lib/ollama/runners/rocm_*
       runHook postUnpack
@@ -65,168 +67,3 @@ self: super: {
     };
   };
 }
-
-# self: super: {
-#   ollama-fix = super.stdenv.mkDerivation rec {
-#     pname = "ollama";
-#     version = "latest";
-#
-#     src = super.fetchurl {
-#       url = let
-#         arch = super.stdenv.hostPlatform.uname.processor;
-#         mappedArch = {
-#           "x86_64" = "amd64";
-#           "aarch64" = "arm64";
-#         }.${arch} or (throw "Unsupported architecture: ${arch}");
-#       in "https://ollama.com/download/ollama-linux-${mappedArch}.tgz";
-#       # Replace with actual hash using nix-prefetch-url
-#       sha256 = "sha256-jU4FS8US1TEVB0wZ3lp5Fd94GA+JwBSruuRJfkqBOjw=";
-#     };
-#
-#     nativeBuildInputs = [
-#       super.autoPatchelfHook
-#       super.makeWrapper
-#       super.gnutar
-#       super.gzip
-#     ];
-#
-#     buildInputs = with super.cudaPackages; [
-#       cuda_cudart
-#       libcublas
-#       # Add other required CUDA packages here if needed
-#     ];
-#
-#     installPhase = ''
-#       mkdir -p $out/bin
-#       mkdir -p ./unpacked
-#       tar xzf $src --directory ./unpacked
-#       mv ./unpacked/ollama $out/bin/
-#       mv ./unpacked/lib/ollama $out/lib/
-#     '';
-#
-#     meta = with super.lib; {
-#       description = "Ollama language model runner";
-#       homepage = "https://ollama.com";
-#       license = licenses.mit;
-#       platforms = [ "x86_64-linux" ];
-#       mainProgram = "ollama";
-#     };
-#   };
-# }
-
-
-################################################################################
-################################################################################
-################################################################################
-
-
-# self: super: {
-#   ollama-cuda-fix = super.stdenv.mkDerivation rec {
-#     pname = "ollama-cuda";
-#     version = "0.5.7";
-#
-#     src = super.fetchurl {
-#       url = let
-#         arch = super.stdenv.hostPlatform.uname.processor;
-#         mappedArch = {
-#           "x86_64" = "amd64";
-#           "aarch64" = "arm64";
-#         }.${arch} or (throw "Unsupported architecture: ${arch}");
-#       in "https://ollama.com/download/ollama-linux-${mappedArch}.tgz";
-#       # Get hash via: nix-prefetch-url "https://ollama.com/download/ollama-linux-amd64.tgz"
-#       hash = "sha256-jU4FS8US1TEVB0wZ3lp5Fd94GA+JwBSruuRJfkqBOjw=";
-#     };
-#
-#     nativeBuildInputs = [
-#       super.autoPatchelfHook
-#       super.makeWrapper
-#       super.gnutar
-#       super.gzip
-#     ];
-#
-#     buildInputs = with super.cudaPackages; [
-#       cuda_cudart
-#       libcublas
-#     ];
-#
-#     # Explicit unpack command
-#     unpackPhase = ''
-#       runHook preUnpack
-#       mkdir -p $out
-#       tar xzf $src -C $out --strip-components=1
-#       runHook postUnpack
-#     '';
-#
-#     # No need for installPhase since we unpack directly to $out
-#     dontInstall = true;
-#
-#     postFixup = ''
-#       wrapProgram $out/bin/ollama \
-#         --prefix LD_LIBRARY_PATH : ${super.lib.makeLibraryPath [
-#           super.cudaPackages.cuda_cudart
-#           super.cudaPackages.libcublas
-#         ]}
-#     '';
-#
-#     meta = with super.lib; {
-#       description = "Ollama with CUDA support using precompiled binaries";
-#       homepage = "https://ollama.com";
-#       license = licenses.mit;
-#       platforms = [ "x86_64-linux" "aarch64-linux" ];
-#       mainProgram = "ollama";
-#     };
-#   };
-# }
-
-# self: super: {
-#   ollama-cuda-fix = super.stdenv.mkDerivation rec {
-#     pname = "ollama-cuda";
-#     version = "0.5.7";
-#
-#     src = super.fetchurl {
-#       url = let
-#         arch = super.stdenv.hostPlatform.uname.processor;
-#         mappedArch = {
-#           "x86_64" = "amd64";
-#           "aarch64" = "arm64";
-#         }.${arch} or (throw "Unsupported architecture: ${arch}");
-#       in "https://ollama.com/download/ollama-linux-${mappedArch}.tgz?version=${version}";
-#       # Get the correct hash using: nix-prefetch-url "https://ollama.com/download/ollama-linux-${mappedArch}.tgz?version=0.5.7"
-#       hash = "sha256-jU4FS8US1TEVB0wZ3lp5Fd94GA+JwBSruuRJfkqBOjw="; # Replace with actual hash
-#     };
-#
-#     nativeBuildInputs = [
-#       super.autoPatchelfHook
-#       super.makeWrapper
-#     ];
-#
-#     buildInputs = with super.cudaPackages; [
-#       cuda_cudart
-#       libcublas
-#     ];
-#
-#     installPhase = ''
-#       mkdir -p $out/bin
-#       install -Dm755 ollama $out/bin/ollama
-#
-#       if [ -d lib ]; then
-#         mkdir -p $out/lib
-#         cp -r lib/* $out/lib/
-#       fi
-#
-#       wrapProgram $out/bin/ollama \
-#         --prefix LD_LIBRARY_PATH : ${super.lib.makeLibraryPath [
-#           super.cudaPackages.cuda_cudart
-#           super.cudaPackages.libcublas
-#         ]}
-#     '';
-#
-#     meta = with super.lib; {
-#       description = "Ollama with CUDA support using precompiled binaries";
-#       homepage = "https://ollama.com";
-#       license = licenses.mit;
-#       platforms = [ "x86_64-linux" "aarch64-linux" ];
-#       mainProgram = "ollama";
-#     };
-#   };
-# }
